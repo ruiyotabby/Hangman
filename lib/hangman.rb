15c31937e4c0801b7abe93dc9a6ca3d090f2c46a
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-10 21:52:09 +0300

    Add a method which loads a yaml file

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,31 +1,41 @@
 require 'yaml'
 
 class Hangman
-  attr_accessor :word, :count
+  attr_accessor :word, :count, :read_file
+
+  def initialize
+    @incorrect_letters = []
+    @word = random_word
+    @count = 8
+    @guess = ''
+    @read_file = File.open('saved.yaml', 'r') { |f| f.read }
+  end
 
   def start
     print 'Welcome to Hangman. Hangman is a guessing game for two or more players. One player thinks of a word, phrase '
     print 'or sentence and the other(s) tries to guess it by suggesting letters within a certain number of guesses. '
-    @incorrect_letters = []
-    @word = random_word
     puts 'You will have 8 incorrect guesses. A word has been chosen, try to guess it. Goodluck.'
-    @count = 8
-    @guess = ''
     word.length.times { @guess.concat('_ ') }
+    print "Press '1' to load game " if read_file.length > 1
+    input = gets.chomp if read_file.length > 1
+    load if input == '1'
     puts check_input
   end
 
   def check_input
     while count >= 1
-      print 'Enter a letter which you think is in the word: (Press 1 to save)'
+      print "Enter a letter which you think is in the word: (Press '1' to save)"
       letter = gets.chomp.downcase[0]
       return save if letter == '1'
 
       puts "Incorrect letters chosen are #{@incorrect_letters}" if @incorrect_letters.length.positive?
-      return check_letter(letter) if word.include?(letter) == false || letter == nil.to_s
+      return check_letter(letter) if word.include?(letter.to_s) == false || letter == nil.to_s
 
       @guess = update_guess(letter)
-      return "You won! The word was '#{word}'" unless @guess.include?('_')
+      unless @guess.include?('_')
+        File.open('saved.yaml', 'w') { |f| f.write '' }
+        return "You won! The word was '#{word}'"
+      end
 
       puts @guess
     end
@@ -33,15 +43,30 @@ class Hangman
   end
 
   def save
-    yaml = YAML.dump(self)
+    yaml = YAML.dump({
+      incorrect_letters: @incorrect_letters,
+      word: @word,
+      count: @count,
+      guess: @guess
+    })
     File.open('saved.yaml', 'w') { |f| f.write(yaml) }
     exit
+  end
+
+  def load
+    yaml = File.open('saved.yaml', 'r') { |f| f.read }
+    obj = Psych.unsafe_load(yaml)
+    @incorrect_letters = obj[:incorrect_letters]
+    @word = obj[:word]
+    @count = obj[:count].to_i
+    @guess = obj[:guess]
   end
 
   def check_letter(letter)
     @incorrect_letters << letter
     @count -= 1
     puts "The letter chosen was not in the word, you have #{count} incorrect guesses left."
+    puts @guess
     check_input
   end
 
