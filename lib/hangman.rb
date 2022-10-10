4abb198d626d8d9cdc6be7070906be5e09142578
Ruiyotabby <ruiyotabby@gmail.com>
2022-10-10 20:40:00 +0300

    Add a method which saves game to a file

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,37 +1,56 @@
+require 'yaml'
+
 class Hangman
+  attr_accessor :word, :count
+
   def start
     print 'Welcome to Hangman. Hangman is a guessing game for two or more players. One player thinks of a word, phrase '
     print 'or sentence and the other(s) tries to guess it by suggesting letters within a certain number of guesses. '
-    word = random_word
+    @incorrect_letters = []
+    @word = random_word
     puts 'You will have 8 incorrect guesses. A word has been chosen, try to guess it. Goodluck.'
-    n = 8
-    guess = ''
-    word.length.times { guess.concat('_ ') }
-    puts check_input(guess, n, word)
+    @count = 8
+    @guess = ''
+    word.length.times { @guess.concat('_ ') }
+    puts check_input
   end
 
-  def check_input(guess, n, word)
-    while n >= 1
-      print "\nEnter a letter which you think is in the word: "
-      letter = gets.chomp.downcase
-      if word.include?(letter) == false || letter == nil.to_s
-        n -= 1
-        return check_input(guess, n, word)
-      end
-      guess = update_guess(guess, word, letter)
-      return "You won! The word was '#{word}'" unless guess.include?('_')
+  def check_input
+    while count >= 1
+      print 'Enter a letter which you think is in the word: (Press 1 to save)'
+      letter = gets.chomp.downcase[0]
+      return save if letter == '1'
 
-      puts guess
+      puts "Incorrect letters chosen are #{@incorrect_letters}" if @incorrect_letters.length.positive?
+      return check_letter(letter) if word.include?(letter) == false || letter == nil.to_s
+
+      @guess = update_guess(letter)
+      return "You won! The word was '#{word}'" unless @guess.include?('_')
+
+      puts @guess
     end
-    return 'You ran out of guesses' if n.zero?
+    return 'You lost as you ran out of incorrect guesses.' if count.zero?
   end
 
-  def update_guess(guess, word, letter)
+  def save
+    yaml = YAML.dump(self)
+    File.open('saved.yaml', 'w') { |f| f.write(yaml) }
+    exit
+  end
+
+  def check_letter(letter)
+    @incorrect_letters << letter
+    @count -= 1
+    puts "The letter chosen was not in the word, you have #{count} incorrect guesses left."
+    check_input
+  end
+
+  def update_guess(letter)
     word.chars.each_with_index do |value, index|
-      guess = guess.split(' ').join
-      guess[index] = value if letter == value
+      @guess = @guess.split(' ').join
+      @guess[index] = value if letter == value
     end
-    guess.split('').join(' ')
+    @guess.split('').join(' ')
   end
 
   def random_word
